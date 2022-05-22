c702a66ae564e0a1b52e15d758334bb7dc34c4d9
james5418 <james900425@gmail.com>
2022-05-22 18:04:50 +0800

    [CI/CD] try fastapi testclient

--- model/test_api.py
+++ model/test_api.py
@@ -2,12 +2,16 @@ import requests
 import pytest
 import json
 import re
+from fastapi.testclient import TestClient
+from .server_fastapi import *
+
+client = TestClient(app)
 
 input = {"data": [["app1", "title1", "content1", "category", 10000, 1],
                    ["app2", "title2", "content2", "category", 10000, 2],
                    ["app3", "title2", "content2", "category", 10000, 3]]}
 
-def test_model(ip):
-    res = requests.post(f"http://{ip}", data=json.dumps(input, ensure_ascii=False))
+def test_model():
+    res = client.post("/", json=input)
     assert res.status_code == 200
     assert re.search("^\"\\[[0-9, ]+\\]\"$", res.text) is not None

ef542710522793db6d57ec71592e06a2b32e1b2b
james5418 <james900425@gmail.com>
2022-07-28 17:58:33 +0800

    [test] add test data

--- graphql/example.py
+++ graphql/example.py
@@ -2,8 +2,8 @@ behavior_list = [
     {
         "notification_id": 12345,
 		"user_id": "77",
-		"time" : 111111111111, 
-		"action": "startSort", 
+		"time" : 10000, 
+		"action": "onPin", 
 		"notification_belongs_category": "all",
 		"mode": 0,
 		"battery": 100,
@@ -20,8 +20,8 @@ behavior_list = [
     {
         "notification_id": 67890,
 		"user_id": "88",
-		"time" : 111111111112, 
-		"action": "startSort", 
+		"time" : 15000, 
+		"action": "onPin", 
 		"notification_belongs_category": "all",
 		"mode": 0,
 		"battery": 100,
@@ -44,12 +44,12 @@ notification_list = [
 		"app_name": "12345_instagram", 
 		"app_title": "XXX", 
 		"content": "XXX",
-		"post_time": 5000,
+		"post_time": 10000,
 		"mode": 0,
 		"battery": 100,
 		"is_charging": True,
 		"connectivity": "WIFI",
-		"notification_mode":"Vibration",
+		"notification_mode": "Vibration",
 		"is_screen_on": True,
 		"is_power_save": True,
 		"is_device_idle": True,
@@ -63,17 +63,120 @@ notification_list = [
 		"app_name": "67890_instagram", 
 		"app_title": "XXX", 
 		"content": "XXX",
-		"post_time": 5000,
+		"post_time": 15000,
 		"mode": 0,
 		"battery": 100,
 		"is_charging": True,
 		"connectivity": "WIFI",
-		"notification_mode":"Vibration",
+		"notification_mode": "Vibration",
 		"is_screen_on": True,
 		"is_power_save": True,
 		"is_device_idle": True,
 		"call_state": "Idle",
 		"day_of_week": 7,
+		"time_of_day": 5,
+    },
+]
+
+activity_list = [
+	{
+		"user_id": "88",
+		"time": 9000,
+		"activity_type": 7,
+	},
+	{
+		"user_id": "77",
+		"time": 15000,
+		"activity_type": 3,
+	},
+	{
+		"user_id": "88",
+		"time": 20001,
+		"activity_type": 3,
+	},
+	{
+		"user_id": "77",
+		"time": 5000,
+		"activity_type": 7,
+	},
+]
+
+recent_use_list = [
+	{
+		"user_id": "77",
+		"time": 6000,
+		"action": "open",
+	},
+	{
+		"user_id": "88",
+		"time": 3000,
+		"action": "close",
+	},
+]
+
+ESM_list = [
+	{
+		"user_id": "77",
+	},
+	{
+		"user_id": "88",
+	},
+]
+
+diary_list = [
+	{
+		"user_id": "77",
+		"EsmQ1": "11",
+		"EsmQ2": "22",
+		"EsmQ3": "33",
+		"EsmQ4": "44",
+		"EsmQ5": "55",
+		"EsmQ6": "66",
+	},
+	{
+		"user_id": "88",
+		"EsmQ1": "111",
+		"EsmQ2": "222",
+		"EsmQ3": "333",
+		"EsmQ4": "444",
+		"EsmQ5": "555",
+		"EsmQ6": "666",
+	},
+]
+
+category_list = [
+	{
+		"user_id": "77",
+		"time" : 10000, 
+		"action": "clickCategory", 
+		"category_name": "Weather",
+		"mode": 0,
+		"battery": 100,
+		"is_charging": True,
+		"connectivity": "WIFI",
+		"notification_mode": "Vibration",
+		"is_screen_on": True,
+		"is_power_save": True,
+		"is_device_idle": True,
+		"call_state": "Idle",
+		"day_of_week": 5,
+		"time_of_day": 5,
+    },
+	{
+		"user_id": "88",
+		"time" : 15000, 
+		"action": "clickCategory", 
+		"category_name": "Weather",
+		"mode": 0,
+		"battery": 100,
+		"is_charging": True,
+		"connectivity": "WIFI",
+		"notification_mode": "Vibration",
+		"is_screen_on": True,
+		"is_power_save": True,
+		"is_device_idle": True,
+		"call_state": "Idle",
+		"day_of_week": 5,
 		"time_of_day": 5,
     },
 ]
--- graphql/test_graphql.py
+++ graphql/test_graphql.py
@@ -2,13 +2,44 @@ import pytest
 
 from main import schema
 from firebase_db import db
+from example import behavior_list, notification_list, activity_list, recent_use_list, ESM_list, diary_list, category_list
 
 
 @pytest.fixture(scope="module")
 def test_data():
-    print("create data")    
+    db.collection(u'behavior_test').document(u'test1').set(behavior_list[0])
+    db.collection(u'behavior_test').document(u'test2').set(behavior_list[1])
+    db.collection(u'notification_test').document(u'test1').set(notification_list[0])
+    db.collection(u'notification_test').document(u'test2').set(notification_list[1])
+    db.collection(u'activity_test').document(u'test1').set(activity_list[0])
+    db.collection(u'activity_test').document(u'test2').set(activity_list[1])
+    db.collection(u'activity_test').document(u'test3').set(activity_list[2])
+    db.collection(u'activity_test').document(u'test4').set(activity_list[3])
+    db.collection(u'recent_use_test').document(u'test1').set(recent_use_list[0])
+    db.collection(u'recent_use_test').document(u'test2').set(recent_use_list[1])
+    db.collection(u'ESM_test').document(u'test1').set(ESM_list[0])
+    db.collection(u'ESM_test').document(u'test2').set(ESM_list[1])
+    db.collection(u'diary_test').document(u'test1').set(diary_list[0])
+    db.collection(u'diary_test').document(u'test2').set(diary_list[1])
+    db.collection(u'category_test').document(u'test1').set(category_list[0])
+    db.collection(u'category_test').document(u'test2').set(category_list[1])
     yield
-    print("delete data")
+    db.collection(u'behavior_test').document(u'test1').delete()
+    db.collection(u'behavior_test').document(u'test2').delete()
+    db.collection(u'notification_test').document(u'test1').delete()
+    db.collection(u'notification_test').document(u'test2').delete()
+    db.collection(u'activity_test').document(u'test1').delete()
+    db.collection(u'activity_test').document(u'test2').delete()
+    db.collection(u'activity_test').document(u'test3').delete()
+    db.collection(u'activity_test').document(u'test4').delete()
+    db.collection(u'recent_use_test').document(u'test1').delete()
+    db.collection(u'recent_use_test').document(u'test2').delete()
+    db.collection(u'ESM_test').document(u'test1').delete()
+    db.collection(u'ESM_test').document(u'test2').delete()
+    db.collection(u'diary_test').document(u'test1').delete()
+    db.collection(u'diary_test').document(u'test2').delete()
+    db.collection(u'category_test').document(u'test1').delete()
+    db.collection(u'category_test').document(u'test2').delete()
 
 
 def test_esm_query(test_data):
